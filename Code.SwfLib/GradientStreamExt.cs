7b301715b2bc4eb2a624596be7d78504273b9458
savs <none@none>
2010-06-25 17:23:16 +0000

    tag serialization

---
+++ Code.SwfLib/GradientStreamExt.cs
@@ -0,0 +1,29 @@
+﻿using Code.SwfLib.Data.Gradients;
+
+namespace Code.SwfLib
+{
+    public static class GradientStreamExt
+    {
+
+        public static GradientRGB ReadGradientRGB(this SwfStreamReader reader)
+        {
+            GradientRGB gradient = new GradientRGB();
+            gradient.SpreadMode = (SpreadMode)reader.ReadUnsignedBits(2);
+            gradient.InterpolationMode = (InterpolationMode)reader.ReadUnsignedBits(2);
+            var count = reader.ReadUnsignedBits(4);
+            for (var i = 0; i < count; i++)
+            {
+                reader.ReadGradientRecordRGB();
+            }
+            return gradient;
+        }
+
+        public static GradientRecordRGB ReadGradientRecordRGB(this SwfStreamReader reader)
+        {
+            GradientRecordRGB record;
+            record.Ratio = reader.ReadByte();
+            record.Color = reader.ReadRGB();
+            return record;
+        }
+    }
+}
