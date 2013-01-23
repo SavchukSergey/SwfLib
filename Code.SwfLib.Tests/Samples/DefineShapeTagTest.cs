02b1152bff99e803bed2ea24967c1a9e5144eed6
Sergey Savchuk <none@none>
2013-01-23 12:02:48 +0200

    DefineShape test

--- Code.SwfLib.Tests/Samples/DefineShapeTagTest.cs
+++ Code.SwfLib.Tests/Samples/DefineShapeTagTest.cs
@@ -9,8 +9,7 @@ namespace Code.SwfLib.Tests.Samples {
     public class DefineShapeTagTest : BaseSampleTest {
 
         [Test]
-        public void Test1()
-        {
+        public void Test1() {
             var tag = ReadTag<DefineShapeTag>("Sample - 1.swf", "0bc5103606398401fa8a9ed11bb0596c");
             Assert.IsNotNull(tag);
             Assert.AreEqual(517, tag.ShapeID);
@@ -51,6 +50,45 @@ namespace Code.SwfLib.Tests.Samples {
             Assert.AreEqual(12, curveShape.ControlDeltaY);
             Assert.AreEqual(9, curveShape.AnchorDeltaX);
             Assert.AreEqual(8, curveShape.AnchorDeltaY);
+        }
+
+        [Test]
+        public void Test2() {
+            var tag = ReadTag<DefineShapeTag>("Sample - 1.swf", "1a542a04b0196e306aab90b755982e0d");
+            Assert.IsNotNull(tag);
+            Assert.AreEqual(511, tag.ShapeID);
+            Assert.AreEqual(2, tag.ShapeBounds.XMin);
+            Assert.AreEqual(41, tag.ShapeBounds.XMax);
+            Assert.AreEqual(2, tag.ShapeBounds.YMin);
+            Assert.AreEqual(38, tag.ShapeBounds.YMax);
+
+            Assert.AreEqual(1, tag.FillStyles.Count);
+            Assert.AreEqual(FillStyleType.SolidColor, tag.FillStyles[0].FillStyleType);
+            Assert.AreEqual(255, tag.FillStyles[0].ColorRGB.Red);
+            Assert.AreEqual(255, tag.FillStyles[0].ColorRGB.Green);
+            Assert.AreEqual(255, tag.FillStyles[0].ColorRGB.Blue);
+
+            Assert.AreEqual(0, tag.Shapes.LineStyles.Count);
+
+            Assert.AreEqual(15, tag.Shapes.ShapeRecords.Count);
+            var firstShape = tag.Shapes.ShapeRecords.First() as StyleChangeShapeRecord;
+            var lastShape = tag.Shapes.ShapeRecords.Last() as EndShapeRecord;
+            Assert.IsNotNull(firstShape);
+            Assert.IsNotNull(lastShape);
+
+            Assert.AreEqual(41, firstShape.MoveDeltaX);
+            Assert.AreEqual(20, firstShape.MoveDeltaY);
+            Assert.IsNull(firstShape.FillStyle0);
+            Assert.IsNotNull(firstShape.FillStyle1);
+            Assert.AreEqual(1, firstShape.FillStyle1.Value);
+
+            var styleChange = tag.Shapes.ShapeRecords[9] as StyleChangeShapeRecord;
+            Assert.IsNotNull(styleChange);
+
+            Assert.AreEqual(31, styleChange.MoveDeltaX);
+            Assert.AreEqual(11, styleChange.MoveDeltaY);
+            Assert.IsNull(styleChange.FillStyle0);
+            Assert.IsNull(styleChange.FillStyle1);
         }
     }
 }
