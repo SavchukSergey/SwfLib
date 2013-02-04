77142262a0ed3d273cd503968c8870a8593015f0
Sergey Savchuk <none@none>
2013-02-04 17:08:14 +0200

    EndTag test

---
+++ Code.SwfLib.Tests/Tags/ControlTags/EndTagTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Code.SwfLib.Tags;
+using Code.SwfLib.Tags.ControlTags;
+using NUnit.Framework;
+
+namespace Code.SwfLib.Tests.Tags.ControlTags {
+    [TestFixture]
+    public class EndTagTest : TestFixtureBase {
+
+        private static readonly byte[] _etalon = new byte[] { };
+
+        [Test]
+        public void ReadTest() {
+            var mem = new MemoryStream(_etalon);
+            var tagReader = new SwfTagDeserializer(new SwfFile());
+            var tagData = new SwfTagData {
+                Type = SwfTagType.End,
+                Data = mem.ToArray()
+            };
+            var res = tagReader.ReadTag<EndTag>(tagData);
+            Assert.IsNotNull(res);
+        }
+
+        [Test]
+        public void WriteTest() {
+            var tag = new EndTag();
+
+            var tagWriter = new SwfTagSerializer(new SwfFile());
+            var tagData = tagWriter.GetTagData(tag);
+
+            Assert.AreEqual(_etalon, tagData.Data);
+        }
+    }
+}
