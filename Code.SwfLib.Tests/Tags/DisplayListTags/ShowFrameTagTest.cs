992e725d14db5f46eb01a3f37d814cde02e30305
Sergey Savchuk <none@none>
2013-02-04 17:11:28 +0200

    ShowFrame test

---
+++ Code.SwfLib.Tests/Tags/DisplayListTags/ShowFrameTagTest.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Code.SwfLib.Tags;
+using Code.SwfLib.Tags.DisplayListTags;
+using NUnit.Framework;
+
+namespace Code.SwfLib.Tests.Tags.DisplayListTags {
+    [TestFixture]
+    public class ShowFrameTagTest : TestFixtureBase {
+
+        private static readonly byte[] _etalon = new byte[] { };
+
+        [Test]
+        public void ReadTest() {
+            var mem = new MemoryStream(_etalon);
+            var tagReader = new SwfTagDeserializer(new SwfFile());
+            var tagData = new SwfTagData {
+                Type = SwfTagType.ShowFrame,
+                Data = mem.ToArray()
+            };
+            var res = tagReader.ReadTag<ShowFrameTag>(tagData);
+            Assert.IsNotNull(res);
+        }
+
+        [Test]
+        public void WriteTest() {
+            var tag = new ShowFrameTag();
+
+            var tagWriter = new SwfTagSerializer(new SwfFile());
+            var tagData = tagWriter.GetTagData(tag);
+
+            Assert.AreEqual(_etalon, tagData.Data);
+        }
+
+    }
+}
