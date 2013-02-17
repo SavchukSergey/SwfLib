33ef82c7160cf4fda51de7b234e640e42eb8521b
Sergey Savchuk <none@none>
2013-02-17 11:14:18 +0200

    Actions tests

---
+++ Code.SwfLib.Tests/Actions/IfActionTest.cs
@@ -0,0 +1,21 @@
+﻿using Code.SwfLib.Actions;
+using NUnit.Framework;
+
+namespace Code.SwfLib.Tests.Actions {
+    public class IfActionTest : BaseActionTest {
+
+        private readonly byte[] _etalon = new byte[] { 0x9d, 0x02, 0x00, 0x01, 0x00 };
+
+        [Test]
+        public void ReadTest() {
+            var action = ReadAction<ActionIf>(_etalon);
+            Assert.IsNotNull(action);
+            Assert.AreEqual(action.BranchOffset, 1);
+        }
+
+        [Test]
+        public void WriteTest() {
+            WriteAction(new ActionIf { BranchOffset = 1 }, _etalon);
+        }
+    }
+}
