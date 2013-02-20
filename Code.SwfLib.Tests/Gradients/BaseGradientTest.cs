205b91eab6eb89e687ef486fcb4d7e0e4a312c06
Sergey Savchuk <none@none>
2013-02-20 11:18:51 +0200

    Gradients test

---
+++ Code.SwfLib.Tests/Gradients/BaseGradientTest.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Code.SwfLib.Tests.Gradients {
+    public abstract class BaseGradientTest<T> {
+
+        protected T ReadGradient(byte[] source) {
+            var mem = new MemoryStream();
+            mem.Write(source, 0, source.Length);
+            mem.Seek(0, SeekOrigin.Begin);
+            var reader = new SwfStreamReader(mem);
+            var gradient = Read(reader);
+            Assert.AreEqual(mem.Length, mem.Position, "Should reach end of stream");
+            return gradient;
+        }
+
+        protected void WriteGradient(T gradient, byte[] etalon)  {
+            var mem = new MemoryStream();
+            var writer = new SwfStreamWriter(mem);
+            Write(writer, gradient);
+            var buffer = mem.ToArray();
+            Assert.AreEqual(etalon, buffer);
+        }
+
+        protected abstract T Read(ISwfStreamReader reader);
+        protected abstract void Write(ISwfStreamWriter writer, T gradient);
+    }
+}
--- Code.SwfLib.Tests/Gradients/GradientRGBATest.cs
+++ Code.SwfLib.Tests/Gradients/GradientRGBATest.cs
@@ -4,13 +4,13 @@ using NUnit.Framework;
 
 namespace Code.SwfLib.Tests.Gradients {
     [TestFixture]
-    public class GradientRGBTest : BaseGradientTest<GradientRGB> {
+    public class GradientRGBATest : BaseGradientTest<GradientRGBA> {
 
         private readonly byte[] _etalon = new byte[] { 0x92,
             0, //Ratio 0 
-            10, 150, 155, // Color 0,
+            10, 150, 155, 128,// Color 0,
             100, // Ratio 1,
-            120, 50, 55, //Color 2
+            120, 50, 55, 192 //Color 2
         };
 
         [Test]
@@ -25,23 +25,23 @@ namespace Code.SwfLib.Tests.Gradients {
             WriteGradient(gradient, _etalon);
         }
 
-        private static GradientRGB GetGradient() {
-            return new GradientRGB {
+        private static GradientRGBA GetGradient() {
+            return new GradientRGBA {
                 InterpolationMode = InterpolationMode.Linear,
                 SpreadMode = SpreadMode.Repeat,
                 GradientRecords = {
-                    new GradientRecordRGB {Ratio = 0, Color = {Red = 10, Green = 150, Blue = 155}},
-                    new GradientRecordRGB {Ratio = 100, Color = {Red = 120, Green = 50, Blue = 55}},
+                    new GradientRecordRGBA {Ratio = 0, Color = {Red = 10, Green = 150, Blue = 155, Alpha = 128}},
+                    new GradientRecordRGBA {Ratio = 100, Color = {Red = 120, Green = 50, Blue = 55, Alpha = 192}},
                 }
             };
         }
 
-        protected override GradientRGB Read(ISwfStreamReader reader) {
-            return reader.ReadGradientRGB();
+        protected override GradientRGBA Read(ISwfStreamReader reader) {
+            return reader.ReadGradientRGBA();
         }
 
-        protected override void Write(ISwfStreamWriter writer, GradientRGB gradient) {
-            writer.WriteGradientRGB(gradient);
+        protected override void Write(ISwfStreamWriter writer, GradientRGBA gradient) {
+            writer.WriteGradientRGBA(gradient);
         }
     }
 }
