802a860ed032943ebcebe443735170fe5b6b0e30
Sergey Savchuk <none@none>
2013-02-20 08:28:10 +0200

    Gradient test

---
+++ Code.SwfLib.Tests/Gradients/GradientRGBTest.cs
@@ -0,0 +1,24 @@
+﻿using Code.SwfLib.Gradients;
+using NUnit.Framework;
+
+namespace Code.SwfLib.Tests.Gradients {
+    [TestFixture]
+    public class GradientRGBTest {
+
+        private byte[] _etalon = new byte[] {};
+        
+        public void ReadTest() {
+        }
+
+        private static GradientRGB GetGradient() {
+            return new GradientRGB {
+                InterpolationMode = InterpolationMode.Linear,
+                SpreadMode = SpreadMode.Repeat,
+                GradientRecords = {
+                    new GradientRecordRGB {Ratio = 0, Color = {Red = 10, Green = 150, Blue = 155}},
+                    new GradientRecordRGB {Ratio = 100, Color = {Red = 120, Green = 50, Blue = 55}},
+                }
+            };
+        }
+    }
+}
