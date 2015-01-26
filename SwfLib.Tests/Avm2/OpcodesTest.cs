bae8c39d18de1543d4a3336bfb2877f651027413
Sergey Savchuk <savchuk.sergey@gmail.com>
2015-01-26 09:20:29 +0200

    avm2 opcodes

---
+++ SwfLib.Tests/Avm2/OpcodesTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SwfLib.Avm2.Opcodes;
+
+namespace SwfLib.Tests.Avm2 {
+    [TestFixture]
+    public class OpcodesTest {
+
+        [Test]
+        public void BaseClassTest() {
+            var baseType = typeof(BaseAvm2Opcode);
+            var ns = baseType.Namespace ?? "";
+
+            var allTypes = baseType.Assembly.GetTypes();
+            foreach (var type in allTypes) {
+                if (type.FullName.StartsWith(ns) && type.IsClass && !type.IsAbstract) {
+                    Assert.IsTrue(type.Name.EndsWith("Opcode"), type.FullName);
+                    Assert.IsTrue(baseType.IsAssignableFrom(type), type.FullName);
+                }
+            }
+        }
+    }
+}
